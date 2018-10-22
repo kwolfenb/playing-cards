3798afa66e85e8d5d7e0e6146032792b885fc968
Gulzat Karimova and Kenny Wolfenberger <kenny@>
2018-10-22 16:41:59 -0700

    initial files

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function(event) {
+  // event.preventDefault();
+
+  var suits = ["diamonds", "spades", "hearts", "clubs"]
+  var values = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, "Jack", "Queen", "King"]
+
+
+ values.forEach(function(value){
+  return value 
+})
+
+  suits.forEach(function(suit) {
+    var result = suit.concat(cards);
+
+
+
+    $(".list").append(values);
+  });
