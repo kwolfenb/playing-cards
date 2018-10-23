3ae380b6770535c72b7015fddc64d14895b990d2
Kenny Wolfenberger <kennywolfenberger@gmail.com>
2018-10-22 20:17:20 -0700

    updates to make fully functional

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,14 @@ $(document).ready(function(event) {
   // event.preventDefault();
 
   var suits = ["diamonds", "spades", "hearts", "clubs"]
-  var values = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, "Jack", "Queen", "King"]
+  var values = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "Jack", "Queen", "King"]
 
 
  values.forEach(function(value){
   suits.forEach(function(suit) {
-    $(".list").text(suit + "of" + value);
-
-
+    $(".list").append("<li>" + value + " of " + suit + "</li>");
   });
-});
+  });
 
 
   });
